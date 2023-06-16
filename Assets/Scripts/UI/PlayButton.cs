82c098e6a0e9f34de57bd4ff37566b051b27a69a
Yavuz Kopar <kopar.yavuz@gmail.com>
2023-06-16 14:58:27 +0300

    LoadingScene

--- Assets/Scripts/UI/PlayButton.cs
+++ Assets/Scripts/UI/PlayButton.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayButton : MonoBehaviour
 {
+    [SerializeField] Slider slider;
+    [SerializeField] GameObject loadingScene;
     public void StartGame()
     {
        GameModeChanger modeChanger = FindObjectOfType<GameModeChanger>();
@@ -12,7 +15,19 @@ public class PlayButton : MonoBehaviour
     }
     public void LoadScene(GameModeChanger.GameMode gameMode)
     {
+        loadingScene.SetActive(true);
         int sceneIndex = (int)gameMode;
-        SceneManager.LoadScene(sceneIndex + 1);
+        StartCoroutine(LoadAsynchronously(sceneIndex + 1));
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
 }
